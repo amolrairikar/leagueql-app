0c36bcfaa9b4aeaaef97ba0909d0c1c6b3b148b7
Amol Rairikar <arairikar1@gmail.com>
2026-03-19 11:13:14 -0500

    fix: adding tags to replica table

--- infrastructure/modules/dynamodb/main.tf
+++ infrastructure/modules/dynamodb/main.tf
@@ -9,13 +9,14 @@ terraform {
 }
 
 resource "aws_dynamodb_table" "global_table" {
-  provider         = aws.primary
-  name             = var.table_name
-  billing_mode     = "PAY_PER_REQUEST"
-  hash_key         = var.hash_key
-  range_key        = var.range_key
-  stream_enabled   = true
-  stream_view_type = "NEW_AND_OLD_IMAGES"
+  provider                    = aws.primary
+  name                        = var.table_name
+  deletion_protection_enabled = true
+  billing_mode                = "PAY_PER_REQUEST"
+  hash_key                    = var.hash_key
+  range_key                   = var.range_key
+  stream_enabled              = true
+  stream_view_type            = "NEW_AND_OLD_IMAGES"
 
   attribute {
     name = var.hash_key
@@ -42,4 +43,23 @@ resource "aws_dynamodb_table" "global_table" {
   }
 
   tags = var.tags
+}
+
+resource "aws_dynamodb_tag" "replica_tags" {
+  provider     = aws.replica
+  resource_arn = replace(aws_dynamodb_table.global_table.arn, data.aws_region.primary.name, var.replica_regions[0])
+  depends_on   = [aws_dynamodb_table.global_table]
+  
+  dynamic "tag" {
+    for_each = var.tags
+    content {
+      key   = tag.key
+      value = tag.value
+    }
+  }
+}
+
+# Helper to get the primary region name for the ARN replacement
+data "aws_region" "primary" {
+  provider = aws.primary
 }
