32a5fe740d0280128efe5385e431d4ea17049083
Amol Rairikar <arairikar1@gmail.com>
2026-04-01 11:13:49 -0500

    feat: deploying ACM certificate

--- infrastructure/regional/main.tf
+++ infrastructure/regional/main.tf
@@ -145,3 +145,14 @@ resource "aws_cloudwatch_log_resource_policy" "apigateway_log_delivery" {
     ]
   })
 }
+
+resource "aws_acm_certificate" "api_subdomain_cert" {
+  domain_name       = "api.leagueql.com"
+  validation_method = "DNS"
+  tags = {
+    environment = var.environment
+    project     = "fantasy-football-recap"
+    component   = "api"
+    managed-by  = "terraform"
+  }
+}
