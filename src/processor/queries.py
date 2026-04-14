022224853ef7cd6e7db3e2dfb2e1121b2d18542d
Amol Rairikar <arairikar1@gmail.com>
2026-04-14 15:59:58 -0500

    feat: updating fields for standings query

--- src/processor/queries.py
+++ src/processor/queries.py
@@ -83,6 +83,7 @@ QUERIES = {
             team_a_score AS points_for,
             CAST(team_b_score AS DOUBLE) AS points_against
         FROM matchups_output
+        WHERE playoff_tier_type = 'NONE'
         UNION ALL
         SELECT 
             season,
@@ -93,6 +94,7 @@ QUERIES = {
             CAST(team_b_score AS DOUBLE) AS points_for,
             team_a_score AS points_against
         FROM matchups_output
+        WHERE playoff_tier_type = 'NONE'
     ),
     league_rankings AS (
         SELECT 
@@ -100,7 +102,6 @@ QUERIES = {
             RANK() OVER (PARTITION BY season, week ORDER BY points_for DESC) as weekly_rank,
             COUNT(*) OVER (PARTITION BY season, week) as total_teams_that_week
         FROM weekly_stats
-        WHERE playoff_tier_type = 'NONE'
     ),
     processed_performance AS (
         SELECT 
@@ -117,28 +118,33 @@ QUERIES = {
         FROM league_rankings
     )
     SELECT 
-        season,
-        team_id,
-        owner_id,
+        p.season,
+        p.team_id,
+        p.owner_id,
+        t.team_name,
+        t.team_logo,
+        t.display_name AS owner_username,
         COUNT(*) AS games_played,
-        SUM(win) AS wins,
-        SUM(loss) AS losses,
-        SUM(tie) AS ties,
+        SUM(p.win) AS wins,
+        SUM(p.loss) AS losses,
+        SUM(p.tie) AS ties,
         CONCAT(
-            CAST(SUM(win) AS STRING), '-', 
-            CAST(SUM(loss) AS STRING), '-', 
-            CAST(SUM(tie) AS STRING)
+            CAST(SUM(p.win) AS STRING), '-', 
+            CAST(SUM(p.loss) AS STRING), '-', 
+            CAST(SUM(p.tie) AS STRING)
         ) AS record,
-        ROUND(SUM(win) / COUNT(*)::DOUBLE, 3) AS win_pct,
-        SUM(vs_league_wins) AS total_vs_league_wins,
-        SUM(vs_league_losses) AS total_vs_league_losses,
-        ROUND(SUM(vs_league_wins) / (SUM(vs_league_wins) + SUM(vs_league_losses))::DOUBLE, 3) AS win_pct_vs_league,
-        SUM(points_for) AS total_pf,
-        SUM(points_against) AS total_pa,
-        ROUND(AVG(points_for), 2) AS avg_pf,
-        ROUND(AVG(points_against), 2) AS avg_pa
-    FROM processed_performance
-    GROUP BY season, team_id, owner_id
-    ORDER BY season DESC, wins DESC, total_pf DESC;
+        ROUND(SUM(p.win) / COUNT(*)::DOUBLE, 3) AS win_pct,
+        SUM(p.vs_league_wins) AS total_vs_league_wins,
+        SUM(p.vs_league_losses) AS total_vs_league_losses,
+        ROUND(SUM(p.vs_league_wins) / (SUM(p.vs_league_wins) + SUM(p.vs_league_losses))::DOUBLE, 3) AS win_pct_vs_league,
+        SUM(p.points_for) AS total_pf,
+        SUM(p.points_against) AS total_pa,
+        ROUND(AVG(p.points_for), 2) AS avg_pf,
+        ROUND(AVG(p.points_against), 2) AS avg_pa
+    FROM processed_performance p
+    INNER JOIN teams_output t
+        ON (p.team_id = t.team_id AND p.season = t.season)
+    GROUP BY p.season, p.team_id, p.owner_id
+    ORDER BY p.season DESC, p.wins DESC, p.total_pf DESC;
     """,
 }
