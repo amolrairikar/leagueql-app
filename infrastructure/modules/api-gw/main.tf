dc29aad2214f027dcf43352e9730852b6f43839b
Amol Rairikar <arairikar1@gmail.com>
2026-05-21 15:28:14 -0500

    fix: fixing throttling terraform resource

--- infrastructure/modules/api-gw/main.tf
+++ infrastructure/modules/api-gw/main.tf
@@ -49,6 +49,18 @@ resource "aws_apigatewayv2_stage" "default" {
     })
   }
 
+  route_settings {
+    route_key              = "POST /leagues"
+    throttling_burst_limit = 1
+    throttling_rate_limit  = 1
+  }
+
+  route_settings {
+    route_key              = "DELETE /leagues/{leagueId}"
+    throttling_burst_limit = 1
+    throttling_rate_limit  = 1
+  }
+
   lifecycle {
     create_before_destroy = true
   }
@@ -70,24 +82,6 @@ resource "aws_apigatewayv2_authorizer" "clerk" {
   lifecycle {
     create_before_destroy = true
   }
-}
-
-resource "aws_apigatewayv2_route_settings" "onboard_throttle" {
-  api_id     = aws_apigatewayv2_api.this.id
-  stage_name = aws_apigatewayv2_stage.default.name
-  route_key  = "POST /leagues"
-
-  throttling_burst_limit = 1
-  throttling_rate_limit  = 1
-}
-
-resource "aws_apigatewayv2_route_settings" "delete_throttle" {
-  api_id     = aws_apigatewayv2_api.this.id
-  stage_name = aws_apigatewayv2_stage.default.name
-  route_key  = "DELETE /leagues/{leagueId}"
-
-  throttling_burst_limit = 1
-  throttling_rate_limit  = 1
 }
 
 resource "aws_lambda_permission" "api_gateway" {
