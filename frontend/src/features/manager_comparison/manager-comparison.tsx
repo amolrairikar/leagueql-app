acba6342a01f799fcb2bbce64bb31c6051dcee56
Amol Rairikar <arairikar1@gmail.com>
2026-05-27 14:58:02 -0500

    chore: manager comparison mobile accessibility

--- frontend/src/features/manager_comparison/manager-comparison.tsx
+++ frontend/src/features/manager_comparison/manager-comparison.tsx
@@ -470,7 +470,7 @@ function ManagerComparisonInner({
   return (
     <div className="flex flex-1 flex-col p-6 overflow-auto">
       <div className="max-w-275 mx-auto w-full">
-        <div className="grid grid-cols-[minmax(0,1fr)_minmax(0,275px)] gap-4 items-stretch">
+        <div className="grid grid-cols-1 sm:grid-cols-[minmax(0,1fr)_minmax(0,275px)] gap-4 items-stretch">
           <div className="flex flex-col gap-3">
             {/* Manager selectors */}
             <div className="grid grid-cols-[1fr_56px_1fr] items-center">
@@ -581,7 +581,7 @@ function ManagerComparisonInner({
             </div>
           </div>
 
-          <div className="relative">
+          <div className="relative h-96 sm:h-auto">
             <GameLogPanel
               left={L}
               right={R}
@@ -616,7 +616,7 @@ function ManagerComparisonSkeleton() {
     <div className="flex flex-1 flex-col p-6 overflow-auto">
       <div className="max-w-275 mx-auto w-full">
         <Skeleton className="h-4 w-48 mx-auto mb-5" />
-        <div className="grid grid-cols-[minmax(0,1fr)_minmax(0,275px)] gap-4 items-start">
+        <div className="grid grid-cols-1 sm:grid-cols-[minmax(0,1fr)_minmax(0,275px)] gap-4 items-start">
           <div className="flex flex-col gap-3">
             <div className="grid grid-cols-[1fr_56px_1fr] items-center gap-2">
               <Skeleton className="h-9 w-full" />
